aacd01fbbcb22fc35adfa3f6968ec80a4c07660b
zoldyckd <62292501+zoldyckd@users.noreply.github.com>
2025-09-08 00:15:31 +0800

    Trying to do interactions

--- src/main.ts
+++ src/main.ts
@@ -9,43 +9,24 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ', WA.player.tags);
+    console.log('Player tags: ',WA.player.tags)
 
-    // Clock popup (your existing one)
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
+    })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup);
+    WA.room.area.onLeave('clock').subscribe(closePopup)
 
-    // NEW: Samurai NPC popup
-    WA.room.area.onEnter('npc_samurai').subscribe(() => {
-        currentPopup = WA.ui.openPopup(
-            "samuraiPopup",
-            "🗡️ Defender of Malware: Stay vigilant, warrior! The digital world is full of threats.",
-            [
-                {
-                    label: "Got it!",
-                    callback: (popup) => {
-                        popup.close();
-                    }
-                }
-            ]
-        );
-    });
-
-    WA.room.area.onLeave('npc_samurai').subscribe(closePopup);
-
-    // Bootstraps the Scripting API Extra
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
-function closePopup() {
+function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
