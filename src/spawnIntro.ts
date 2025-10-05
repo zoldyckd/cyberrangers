335ef33f2db13164d0275ce96c961bf406fcdbe3
zoldyckd <62292501+zoldyckd@users.noreply.github.com>
2025-10-06 00:49:04 +0800

    Update spawnIntro.ts

--- src/spawnIntro.ts
+++ src/spawnIntro.ts
@@ -14,7 +14,7 @@ function openSpawnIntro() {
 
   spawnPopupRef = WA.ui.openPopup(
     "spawnIntroPopup",
-    "👋 Welcome! Use the Arrow Keys or WASD to move. Explore the map and look for the wooden signage for guidance. Tip: Walk close to objects (signs, boards, NPCs) to interact with them.",
+    "👋 Welcome! Use the Arrow Keys or WASD to move. Explore the map and look for Professor Byte for guidance. Tip: Walk close to objects (signs, boards, NPCs) to interact with them.",
     [
       {
         label: "Got it",
