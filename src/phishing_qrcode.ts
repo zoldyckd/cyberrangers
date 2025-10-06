4d559bae08f54447e728c1525174d6b092ad850d
zoldyckd <62292501+zoldyckd@users.noreply.github.com>
2025-10-06 21:21:44 +0800

    Update phishing_qrcode.ts

--- src/phishing_qrcode.ts
+++ src/phishing_qrcode.ts
@@ -11,7 +11,7 @@ export function initphishing_QRcode() {
       closePopup();
       previewRef = WA.ui.openPopup(
         ANCHOR,
-        "🍗 You spot a poster in the library:“FREE NUGGETS FOR LIFE – Scan Me!” Your friend insists it looks real, but scanning random QR codes can lead to fake websites or data theft. What would you do? Press SPACE to help!"",
+        "🍗 You spot a poster in the library:“FREE NUGGETS FOR LIFE – Scan Me!” Your friend insists it looks real, but scanning random QR codes can lead to fake websites or data theft. What would you do? Press SPACE to help!",
         [{ label: "Got it", callback: () => closePopup() }]
       );
     });
